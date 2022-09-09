f691cd77fec118fdc0549860e65e1cbe7625b31d
Andrew Freebairn <andrew.freebairn@csiro.au>
2022-09-09 16:12:27 +1000

    Create demo-model.py

---
+++ demo-model.py
@@ -0,0 +1,21 @@
+
+class demo_model():
+    """ 
+    Demo
+    """
+
+    @staticmethod
+    def ID():
+        return 'demo'
+
+    def __init__(self):
+        '''Load all required input data and parameters'''
+        return True
+
+    def reset(self):
+        '''Reset model parameters'''
+
+    def runtimestep(self):
+        '''Usually this is where science happens, however this is just a demo'''
+        with open('readme.txt', 'w') as f:
+            f.write('demo results')
